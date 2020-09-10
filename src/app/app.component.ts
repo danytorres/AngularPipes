1f96e8299425135a476c3527de36678ededd93ee
danytorresbmx <danytorresbmx@gmail.com>
2020-09-09 21:53:20 -0500

    segunda version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,24 @@ export class AppComponent {
 
   porcentaje: number = 0.234;
 
+  salario: number = 1234.5;
+
+  fecha: Date = new Date();
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  } );
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
 }
